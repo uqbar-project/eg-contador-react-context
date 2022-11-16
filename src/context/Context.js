b41fa665bd3813948acc6564aa28fea932330df7
Fernando Dodino <fernando.dodino@gmail.com>
2022-11-15 21:26:28 -0300

    Better context definition (extracting common fn)

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,80 +1,46 @@
-import PropTypes from 'prop-types'
-import React, { createContext } from 'react'
+import React, { createContext,useState } from 'react'
 
 import { Log } from '../domain/log'
 
 export const Context = createContext()
 
-export class Provider extends React.Component {
-  static propTypes = { children: PropTypes.node.isRequired }
-  state = {
-    logs: [],
-    count: 0
+export const Provider = ({ children }) => {
+  const [count, setCount] = useState(0)
+  const [logs, setLogs] = useState([])
+
+  const addLog = (action) => {
+    // También podemos hacer
+    // const newLogs = [...logs]
+    // newLogs.push(new Log(action))
+    // lo importante es generar una copia
+
+    const newLogs = logs.concat(new Log(action))
+    setLogs(newLogs)
   }
-  decrement = () => {
-    this.setState({
-      logs: this.state.logs.concat(new Log('DECREMENT')),
-      count: this.state.count - 1
-    })
-  }
-  increment = () => {
-    this.setState({
-      logs: this.state.logs.concat(new Log('INCREMENT')),
-      count: this.state.count + 1
-    })
-  }
-  deleteLog = (logToDelete) => {
-    const newLogs = this.state.logs.filter((log) => logToDelete.id !== log.id)
-    this.setState({
-      logs: newLogs
-    })
-  }
-  render() {
-    const value = {
-      count: this.state.count,
-      logs: this.state.logs,
-      decrement: this.decrement,
-      increment: this.increment,
-      deleteLog: this.deleteLog
+
+  const value = {
+    // Publicamos el estado
+    count,
+    logs,
+    // Funciones que afectan al estado
+    decrement: () => {
+      addLog('DECREMENT')
+      setCount(count - 1)
+    },
+    increment: () => {
+      addLog('INCREMENT')
+      setCount(count + 1)
+    },
+    deleteLog: (logToDelete) => {
+      // fíjense que el context está tomando una responsabilidad
+      const newLogs = logs.filter((log) => logToDelete.id !== log.id)
+      setLogs(newLogs)
     }
-    return (
-      <Context.Provider value={value}>
-        {this.props.children}
-      </Context.Provider>
-
-    )
   }
+
+  return (
+    <Context.Provider value={value}>
+      {children}
+    </Context.Provider>
+  )
 }
-
-
-// EJEMPLO DE COMO HACERLO CON COMPONENTES FUNCIONALES
-// import React, { createContext,useState } from 'react'
-// export const Provider = ({ children }) => {
-//     const [count, setCount] = useState(0)
-//     const [logs, setLogs] = useState([])
-//     const value = {
-//         count,
-//         logs,
-//         decrement: () => {
-//             const newLogs = [...logs]
-//             newLogs.push(new Log('DECREMENT'))
-//             setLogs(newLogs)
-//             setCount(count - 1)
-//         },
-//         increment: () => {
-//             const newLogs = [...logs]
-//             newLogs.push(new Log('INCREMENT'))
-//             setLogs(newLogs)
-//             setCount(count + 1)
-//         },
-//         deleteLog: (logToDelete) => {
-//             const newLogs = logs.filter((log) => logToDelete.id !== log.id)
-//             setLogs(newLogs)
-//         }
-//     }
-//     return (
-//         <Context.Provider value={value}>
-//             {children}
-//         </Context.Provider>
-//     )
-// }
